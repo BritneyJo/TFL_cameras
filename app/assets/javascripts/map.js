1ac005b89c5123db6a74d61c5984313a9260a735
bj <britneyjo.ludkowski@gmail.com>
2013-07-31 09:55:06 +0100

    added marker pop up window

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,6 +3,7 @@ $(function(){
       canvas,
       map;
 
+//creates map when first load the page
   mapOptions = {
     zoom:12,
     center:new google.maps.LatLng(51.508742, -0.120850),
@@ -12,13 +13,31 @@ $(function(){
   canvas = document.getElementById("googleMap");
   map = new google.maps.Map(canvas, mapOptions);
 
- var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(51.508742, -0.120850),
-      map: map
-  });
-
+//gets the cameras arry and uses JSON to turn it into readable data
  $.getJSON('/cameras.json', function(data) {
-    console.log(data);
-  });
 
+    //loop through the arrays to create a marker for each camera
+    for(var i = 0; i < data.length; i++){
+      var camera = data[i];
+
+      //paints marker on the map with camera's lat and lng
+      var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(camera.lat ,camera.lng),
+      map: map,
+      camera_id: i,
+      location: camera.location,
+      postcode: camera.postcode,
+      file: camera.file,
+      animation: google.maps.Animation.DROP
+      });
+
+      google.maps.event.addListener(marker, 'click', function(){
+        var contentString = '<div id="content"><img src="http://www.tfl.gov.uk/tfl/livetravelnews/trafficcams/cctv/' + this.file + '"><p>' + this.location + ", " + this.postcode + '</p></div>';
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+        infowindow.open(map, this)
+      });
+    }
+  })
 });
